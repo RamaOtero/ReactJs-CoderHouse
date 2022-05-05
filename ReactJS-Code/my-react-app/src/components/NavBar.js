2d2668a71a514a01142cf70e7e3c3fe5e6cb4a03
Rama Otero <ramirootero98@gmail.com>
2022-05-04 23:27:21 -0300

    CartWigetItemList

--- ReactJS-Code/my-react-app/src/components/NavBar.js
+++ ReactJS-Code/my-react-app/src/components/NavBar.js
@@ -1,35 +1,36 @@
 import logo from '../logo.svg';
+import CartWidget from './CartWidget.Js';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 function NavBar() {
   return (
-    <div className="App">
+    <div className="Navbar">
       <Navbar bg="dark" variant="dark"
         sticky="top" expand="sm" collapseOnSelect>
         <Navbar.Brand>
           <img src={logo} width="40px" height="40px" />{' '}
-          Sneakers
+          ArtBoard
         </Navbar.Brand>
 
         <Navbar.Toggle className="coloring" />
         <Navbar.Collapse>
           <Nav>
-            <NavDropdown title="Productos">
-              <NavDropdown.Item href="#products/tea">Tea</NavDropdown.Item>
-              <NavDropdown.Item href="#products/coffee">Coffee</NavDropdown.Item>
-              <NavDropdown.Item href="#products/chocolate">Chocolate</NavDropdown.Item>
+            <NavDropdown title="Estilos">
+              <NavDropdown.Item href="#products/Digital">Digital</NavDropdown.Item>
+              <NavDropdown.Item href="#products/Clasico">Clasico</NavDropdown.Item>
+              <NavDropdown.Item href="#products/Contemporaneo">Contemporaneo</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#products/promo">Promo</NavDropdown.Item>
+              <NavDropdown.Item href="#products/Graffiti">Graffiti</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#blog">Blog</Nav.Link>
             <Nav.Link href="#about-us">Sobre Nosotros</Nav.Link>
             <Nav.Link href="#contact-us">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-
       </Navbar>
+      
       <div className="content">
         This is a content.
       </div>
