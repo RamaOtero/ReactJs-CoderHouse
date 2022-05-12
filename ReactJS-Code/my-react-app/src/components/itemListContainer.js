f242eb379c29d36480b95d7f57b23021a449573e
Rama Otero <ramirootero98@gmail.com>
2022-05-11 23:35:40 -0300

    Itemlist

--- ReactJS-Code/my-react-app/src/components/itemListContainer.js
+++ ReactJS-Code/my-react-app/src/components/itemListContainer.js
@@ -1,7 +1,9 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import productos from './itemList'
 
 const itemList = ({greetings}) => {
        return (
-   
            <div>
                <h1> Galeria de Arte {greetings} </h1>
            </div>
